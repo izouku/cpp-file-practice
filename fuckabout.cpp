efdb80e60c50e903670b8a43011ae14e2da99e91
David Corner <dave@Davids-MacBook-Pro.local>
2025-12-13 12:11:20 +0000

    Initial Commit

---
+++ fuckabout.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num > 0) {
+        cout << "Positive number" << endl;
+    } else if (num < 0) {
+        cout << "Negative number" << endl;
+    } else {
+        cout << "Zero" << endl;
+    }
+
+    return 0;
+}
